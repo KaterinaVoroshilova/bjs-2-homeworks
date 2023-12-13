02a379bdbc160ad9609dd620fd70b52b8618e15a
Katerina Voroshilova <kotovaekaterina1@gmail.com>
2023-12-13 14:56:50 +0300

    ДЗ  base-concepts

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,19 +1,15 @@
 "use strict"
 function solveEquation(a, b, c) {
   let roots = [];
-  discriminant = Math.pow(b,2) - 4*a*c;
+  let discriminant = Math.pow(b,2) - 4*a*c;
   if (discriminant > 0) {
-    roots[0] = (-b + Math.sqrt(d) )/(2*a);
-    roots[1] = (-b - Math.sqrt(d) )/(2*a);
-    return (roots[0], roots[1])
+    roots[0] = (-b + Math.sqrt(discriminant) )/(2*a);
+    roots[1] = (-b - Math.sqrt(discriminant) )/(2*a);
   } 
   else if (discriminant===0) {
     roots[0] = -b/(2*a)
-    return roots[0];
   }
-  else {
   return roots;
-  }
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
