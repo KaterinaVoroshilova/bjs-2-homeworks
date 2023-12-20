f6d06145e5f54e784bbbafc04c2d9e973de908f1
Katerina Voroshilova <kotovaekaterina1@gmail.com>
2023-12-20 14:09:31 +0300

    Задание 3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,11 @@
 function compareArrays(arr1, arr2) {
-  
+	if (arr1.length !== arr2.length) {
+		return false;
+	} else {
+		return arr1.every((element, index) => element === arr2[index]);
+	}
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+	return users.filter(user => user.gender === gender).map(user => user.age).reduce((acc, age, index, arr) => acc + age / arr.length, 0);
 }
